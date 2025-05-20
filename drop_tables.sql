7eceb67803313e33fe13ddb30325e16b1d33c05d
FTG200625 <f.taefi.g@gmail.com>
2025-05-20 21:56:50 +0330

    Add files via upload

---
+++ drop_tables.sql
@@ -0,0 +1,8 @@
+drop table order_items;
+drop table orders;
+drop table menu_foods;
+drop table daily_menus;
+drop table food_item;
+drop table professors;
+drop table students;
+drop table persons;
